08813f89e588b6df24dbace1fe0c48b74d1443e2
Subhah Prasad Sah <prasadsah500@gmail.com>
2026-04-23 00:55:45 +0530

    modifed the app.py for render

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,10 @@ from api.bb84_routes import bb84_api
 from api.e91_routes import e91_api
 from api.bbm92_routes import bbm92_api
 from api.b92_routes import b92_api
+import os
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.get("/")
 def home():
@@ -21,12 +21,11 @@ def home():
         ],
     }
 
-# Register API blueprints
 app.register_blueprint(bb84_api, url_prefix="/api/bb84")
 app.register_blueprint(e91_api, url_prefix="/api/e91")
 app.register_blueprint(bbm92_api, url_prefix="/api/bbm92")
 app.register_blueprint(b92_api, url_prefix="/api/b92")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
